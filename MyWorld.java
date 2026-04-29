443579101ddc0821aa177f34c484ecb339a00462
DrewDawgs <andrewxchen018@gmail.com>
2026-04-29 11:41:03 -0400

    Revert "Update MyWorld.java"
    This reverts commit 42ccbfb26a30035458ba75d17041e191be4fe17f.

--- MyWorld.java
+++ MyWorld.java
@@ -3,8 +3,5 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-        Bee bee = new Bee();
-        
-        addObject (bee, 300, 300);
     }
 }
