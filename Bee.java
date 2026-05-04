8d53561580623e8e5519b2de457fc5bf81431adf
DrewDawgs <andrewxchen018@gmail.com>
2026-05-04 14:52:11 -0400

    added images and sound effects

--- Bee.java
+++ Bee.java
@@ -8,21 +8,66 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Bee extends Actor
 {
-    /**
-     * Act - do whatever the Bee wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound eatSound = new GreenfootSound("harp-pluck-short-soft-f_F#.wav");
+    GreenfootImage[] idleRight = new GreenfootImage[5];
+    GreenfootImage[] idleLeft = new GreenfootImage[5];
+    
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    public Bee()
+    {
+        for(int i = 0; i < idleRight.length; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/bee_idle/tile00" + i + ".png");
+            idleRight[i].scale(100, 100);
+        }
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/bee_idle/tile00" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        
+        animationTimer.mark();
+    }
+    
+    
+    int imageIndex = 0;
+    public void animateBee()
+    {
+        if(animationTimer.millisElapsed() < 75)
+        {
+            return;
+        }
+        animationTimer.mark();
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+        
+    }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
         {
             move(-1);
+            facing = "left";
         }
         else if(Greenfoot.isKeyDown("right"))
         {
             move(1);
+            facing = "right";
         }
         eat();
+        
+        animateBee();
     }
     //eats apple and spawns new apple when apple is eaten
     public void eat()
@@ -33,6 +78,7 @@ public class Bee extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createOrange();
             world.increaseScore();
+            eatSound.play();
         }
     }
 }
